2c44ddd2edc0f53e09ae2e2f60026c79b20d61d9
ST10507987 <ST10507987@myemeris.edu.za>
2026-02-25 14:18:19 +0200

    Final submission

---
+++ src/first_app_ant_/First_App_AnT_.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package first_app_ant_;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class First_App_AnT_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("             -------------------------------------------------");
+        System.out.println("             -                 !!WARNNING!!                  -");
+        System.out.println("             -------------------------------------------------");
+        
+        
+        System.out.println("               *********                             ******* ");
+        System.out.println("             *           *                         *          *");
+        System.out.println("           *    S T O P    *                     *     G 0      *");
+        System.out.println("             *           *                         *          *");
+        System.out.println("               *********                             *******");
+        System.out.println("                  ***                                  ***");
+        System.out.println("                  ***                                  ***");
+        System.out.println("                  ***                                  ***");
+        System.out.println("                  ***                                  ***");
+        System.out.println("                 *****                                *****");
+        System.out.println("                *******                              *******");
+        System.out.println("               *********                            *********");
+        
+        
+        System.out.println("-----------------------------------------------------------------------------------");
+        System.out.println("-                           !!Follow the RULES!!                                    -");
+        System.out.println("-----------------------------------------------------------------------------------");
+   
+        
+    }
+    
+}
